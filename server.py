9e41cc09cd64bf968b4b7bcd5504740118337b16
kazuki <guruguru9606@gmail.com>
2017-09-18 17:21:45 +0900

    tcp connection is OK

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 import socket
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = socket.gethostname()
+host = '192.168.1.3'
 port = 9999
 serversocket.bind((host, port))
 serversocket.listen(5)
